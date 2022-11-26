7a8965332a5f51bbaabed6dce43bdcd2cd99a3db
Deven Goratela <deven.goratela@tcs.com>
2022-11-26 11:36:32 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aastha Health Plus Physio Tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
