460196d7dc3c506243194d2ab98eeffaa2f6b42d
Deven Goratela <deven.goratela@tcs.com>
2022-12-06 11:27:06 +0530

    gym - wip

---
+++ src/components/GymMemberList.tsx
@@ -0,0 +1,37 @@
+import { IonItemSliding, IonItem, IonAvatar, IonLabel, IonItemOptions, IonItemOption, IonIcon, IonList, IonNote } from "@ionic/react";
+import { createOutline } from "ionicons/icons";
+import _ from "lodash";
+import Avatar from "react-avatar";
+
+const GymMemberList = ({ allGymMembers }: any) => {    
+    return (
+        <IonList>
+            {allGymMembers && allGymMembers.map((member: any) => (
+                <IonItemSliding key={member["🔒 Row ID"]}>
+                    <IonItem button={true} key={member["🔒 Row ID"]} detail={true} 
+                        href={`/viewgymmember/${member["🔒 Row ID"]}`}     
+                        style={{borderLeft: `${member["IsPersonalTraining"]?'3px':'0px'} solid var(--ion-color-secondary)`}}
+                    >
+                        <IonAvatar slot="start">
+                            <Avatar name={member["Name"]} round size="100%" />
+                        </IonAvatar>
+                        <IonLabel>
+                            <h2>{member["Name"]}</h2>
+                            <p>{member["Ending Date"]}</p>
+                        </IonLabel>
+                        <IonLabel slot="end">{(_.toNumber(member["Amount Received"] || 0)) - (_.toNumber(member["Amount Pending"] || 0))}</IonLabel>
+                    </IonItem>
+
+                    <IonItemOptions>
+                        <IonItemOption onClick={() => { window.location.href = `/managegymmember/${member["🔒 Row ID"]}` }}>
+                            <IonIcon slot="top" icon={createOutline} />
+                            Edit
+                        </IonItemOption>
+                    </IonItemOptions>
+                </IonItemSliding>
+            ))}
+        </IonList>
+    );
+}
+
+export default GymMemberList;
