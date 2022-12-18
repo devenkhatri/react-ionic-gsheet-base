f727ec1c2f4082357dd68857a79c1babba3bf8e7
Harshna Goratela <harshna.goratela@gmail.com>
2022-12-18 11:48:50 +0530

    debug lazy loading issue

--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -39,7 +39,7 @@ const Sessions: React.FC = () => {
       setItems(newItems);
     }
   };
-  // console.log("****** items", items)
+  console.log("****** items", items)
 
   useEffect(() => {
     generateItems();
